2a81c2e8d821115ff2f69abf8d09de75a787a194
Damilare <oluwadamilareshobowale@gmail.com>
2019-04-25 12:52:16 +0100

    Updates

---
+++ precache-manifest.62a1978626ece4703ee4f058d6b8a114.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcd238b97aa1382de297",
+    "url": "/techhut-studio-14/static/css/main.5ad45a6b.chunk.css"
+  },
+  {
+    "revision": "fcd238b97aa1382de297",
+    "url": "/techhut-studio-14/static/js/main.b3624756.chunk.js"
+  },
+  {
+    "revision": "de8a66183f92ddde5c6e",
+    "url": "/techhut-studio-14/static/js/runtime~main.299b100a.js"
+  },
+  {
+    "revision": "1624342e36cbb4660c8d",
+    "url": "/techhut-studio-14/static/css/2.07500968.chunk.css"
+  },
+  {
+    "revision": "1624342e36cbb4660c8d",
+    "url": "/techhut-studio-14/static/js/2.e4a526a6.chunk.js"
+  },
+  {
+    "revision": "22bf74a23aa7bef95ff75b155bec6537",
+    "url": "/techhut-studio-14/static/media/arrow-right.22bf74a2.svg"
+  },
+  {
+    "revision": "b3cf76197054583f23d083fde9f27b75",
+    "url": "/techhut-studio-14/static/media/arrow-left.b3cf7619.svg"
+  },
+  {
+    "revision": "5db8bbfb6a548babd3c88584bc301dd2",
+    "url": "/techhut-studio-14/static/media/Bemio.5db8bbfb.otf"
+  },
+  {
+    "revision": "ed524d3938b150b70d120697d516e805",
+    "url": "/techhut-studio-14/static/media/Bemio-Italic.ed524d39.otf"
+  },
+  {
+    "revision": "34d8bd854436b28fd74e333de133a531",
+    "url": "/techhut-studio-14/static/media/lineto-brown-pro-regular.34d8bd85.ttf"
+  },
+  {
+    "revision": "23f9056d875ef44e00eec8b47ec9e22a",
+    "url": "/techhut-studio-14/static/media/lineto-brown-pro-light.23f9056d.ttf"
+  },
+  {
+    "revision": "02068ad19b8f1f4005fdb8e4eb3db3af",
+    "url": "/techhut-studio-14/static/media/lineto-brown-pro-bold.02068ad1.ttf"
+  },
+  {
+    "revision": "5a0ffb993cbb11be48418115ede84146",
+    "url": "/techhut-studio-14/static/media/student-writting.5a0ffb99.png"
+  },
+  {
+    "revision": "e4d0bd25713d6b9683ac0ae107926ef5",
+    "url": "/techhut-studio-14/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/techhut-studio-14/precache-manifest.62a1978626ece4703ee4f058d6b8a114.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/techhut-studio-14/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
